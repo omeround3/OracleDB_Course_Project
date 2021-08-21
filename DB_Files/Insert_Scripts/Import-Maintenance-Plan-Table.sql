67677d403268cc4af3923a85022371049907000e
omeround3 <45568925+omeround3@users.noreply.github.com>
2021-08-21 22:21:38 +0300

    Updated insert scripts for new version of ERD

---
+++ DB_Files/Insert_Scripts/Import-Maintenance-Plan-Table.sql
@@ -0,0 +1,32 @@
+SET DEFINE OFF
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (1, 'approved', 'Monthly cleanup');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (2, 'declined', 'Shower has been renewed recently');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (3, 'waiting', 'Building''s pipes cleanup - to be approved');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (4, 'approved', 'yearly paint job for the building surrounding');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (5, 'approved', 'update security cameras and NVR firmware');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (6, 'declined', 'A new contractor should be found.');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (7, 'declined', 'Not in the budget');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (8, 'approved', 'A new contractor to handle the yard.');
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (9, 'waiting', NULL);
+
+INSERT INTO MAINTENANCE_PLAN (PLAN_ID, STATUS, DESCRIPTION) 
+VALUES (10, 'declined', 'Not in the budget');
+
