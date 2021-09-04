019c8e226dfabd2fadd07a57484ac9298910036f
omeround3 <45568925+omeround3@users.noreply.github.com>
2021-09-04 13:05:22 +0300

    Delete DB_Files directory

--- DB_Files/Views/APARTMENTS_TOTAL_RATES.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW APARTMENTS_TOTAL_RATES
-AS SELECT apartment_id, SUM(rate) total_rate
-FROM apartment_tenants
-GROUP BY apartment_id WITH READ ONLY;
--- DB_Files/Views/COMMITTE_VIEW.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-        (t.first_name ||'  '|| t.last_name) full_name, 
-        t.age, 
-        c.num_supporters
-FROM tenant t 
-LEFT JOIN candidates c ON t.tenant_id = c.candidate_id 
-WHERE c.status = 'elected'
-ORDER BY 
-    c.num_supporters DESC 
