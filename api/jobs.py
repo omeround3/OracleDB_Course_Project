9c7d06ffd49440473d6aa85e7ee7c0363a8d642f
Omer Lev-Ron <omeround3@gmail.com>
2021-09-08 19:03:17 +0300

    Fixed

--- api/jobs.py
+++ api/jobs.py
@@ -14,17 +14,35 @@ jobs_bp = Blueprint('jobs', __name__, url_prefix='/jobs')
 @jobs_bp.route("/", methods=['GET'])
 def jobs():
     """ 
-        A GET request that returns apartments payment information
+        A GET request that returns jobs information
         :param id - exact number of job id
         :return - JSON object that contains jobs information
     """
     job_id = request.args.get('job_id')
     if job_id:
-        sql = """SELECT * FROM jobs WHERE job_id = :job_id"""
+        sql = """
+                SELECT 
+                    j.job_id, 
+                    j.job_type, 
+                    j.description, 
+                    t.first_name || ' ' || t.last_name as tenant_name
+                FROM jobs j, tenant t
+                WHERE j.tenant_id = t.tenant_id
+                AND job_id = :job_id
+              """
         cursor.execute(sql, [job_id])
         r = cursor.fetchall()
         return jsonify(r)
-    cursor.execute("SELECT * FROM jobs")
+    sql = """
+            SELECT 
+                j.job_id, 
+                j.job_type, 
+                j.description, 
+                t.first_name || ' ' || t.last_name as tenant_name
+            FROM jobs j, tenant t
+            WHERE j.tenant_id = t.tenant_id
+            """
+    cursor.execute(sql)
     r = cursor.fetchall()
     return jsonify(r)
 
@@ -68,3 +86,22 @@ def delete_job():
     r = cursor.callproc('DELETE_JOB_PROC', [job_id])
     con.commit()
     return jsonify(r)
+
+@jobs_bp.route('/bids', methods=['GET'])
+def get_bids():
+    """ 
+        A GET request to show jobs bids information, which is basically just the bids of contractors for different kinds of jobs.
+        :param contractor_id - exact number of contractor id
+        :param job_id - exact number of job id
+        :return - JSON object that contains jobs bids information.
+    """
+    contractor_id = request.args.get('contractor_id')
+    job_id = request.args.get('job_id')
+
+    # if contractor_id and job_id:
+        
+    # elif contractor_id:
+    
+    # elif job_id:
+
+    # else:
